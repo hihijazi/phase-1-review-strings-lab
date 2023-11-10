5fcce43a30a0486aeee8743f543fc69b32eda29e
hadil <hihijazi@gmail.com>
2023-11-09 19:39:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'hihijazi';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + 'hihijazi!';
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + 'HIHIJAZI!';
+let shortGreeting = "Welcome, h!";
